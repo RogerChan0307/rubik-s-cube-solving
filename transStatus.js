8767fdbd20a14341f5983ebe37ca2b001839a287
RogerChan0307 <140886171+RogerChan0307@users.noreply.github.com>
2023-12-02 19:05:12 +0800

    Create transStatus.js
    建立用來記錄轉移狀態的物件

---
+++ transStatus.js
@@ -0,0 +1,14 @@
+class TransStatus{
+
+   constructor(){
+    this.to={};
+       this.to['x+']="";
+       this.to['x-']="";
+       this.to['y+']="";
+       this.to['y-']="";
+       this.to['z+']="";
+       this.to['z-']="";
+              
+   }
+    
+}
